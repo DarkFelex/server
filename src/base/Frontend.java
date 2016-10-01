e94078a8598d474f9a637004fda0b5e7848abbae
nmikutskiy <nmikutskiy@yandex-team.ru>
2016-10-01 13:00:46 +0300

    add - frontend and messageSystem interfaces

---
+++ src/base/Frontend.java
@@ -0,0 +1,9 @@
+package base;
+
+/**
+ * Весь набор функциональности, связанный с работой с пользователями
+ *
+ * Created by nmikutskiy on 01.10.16.
+ */
+public interface Frontend {
+}
---
+++ src/base/MessageSystem.java
@@ -0,0 +1,7 @@
+package base;
+
+/**
+ * Created by nmikutskiy on 01.10.16.
+ */
+public interface MessageSystem {
+}
