147abb858f40b0c00a6e5aa9b57a9ae86d713129
nmikutskiy <nmikutskiy@yandex-team.ru>
2016-10-07 16:41:19 +0300

    add - map, create map

--- src/game/Map.java
+++ src/game/Map.java
@@ -1,11 +1,20 @@
 package game;
 
-import java.awt.*;
-
 /**
  * Created by nmikutskiy on 02.10.16.
  */
 public class Map {
+
+    public static void main(String[] args) {
+        Map map = new Map();
+        map.generateMap();
+    }
+
+    public static final int SIZE_X = 15;
+    public static final int SIZE_Y = 15;
+
+    private Place[][] mapField  = new Place[SIZE_X][SIZE_Y];
+
     public Map createMapInDB(){
         return null;
     }
@@ -15,7 +24,27 @@ public class Map {
     }
 
     public Place getPlace(int X, int Y){
-        return new Place();
+        return null;
+    }
+
+    public static void generateTestMap() {
+        int[][] mapField  = new int[SIZE_X][SIZE_Y];
+        for (int i = SIZE_X; i > 0; i--) {
+            for (int j = 1; j <= SIZE_Y; j++) {
+                System.out.print(i + "|" + j + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public void generateMap(){
+        for (int i = SIZE_X; i > 0; i--) {
+            for (int j = 1; j <= SIZE_Y; j++) {
+                mapField[i-1][j-1] = new Place(i, j);
+                System.out.print(mapField[i-1][j-1].toString() + "\t");
+            }
+            System.out.println();
+        }
     }
 
 }
--- src/game/Place.java
+++ src/game/Place.java
@@ -3,17 +3,31 @@ package game;
 import java.awt.*;
 
 /**
- *
  * Клетка на карте
- *
+ * <p>
  * Created by nmikutskiy on 04.10.16.
  */
 public class Place {
-    private int x;
-    private int y;
+    private final int x;
+    private final int y;
     private boolean isPlaceForVillage;
     private Village village;
     private Object resources;
     private Object army;
     private Image placeImage;
+
+    private Place() {
+        x = -1;
+        y = -1;
+    }
+
+    public Place(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%1$02d|%2$02d", x, y);
+    }
 }
